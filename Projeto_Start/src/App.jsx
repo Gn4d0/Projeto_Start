e0258ed23fabfb0878492e83c0d38953eaf34423
Agnaldo Silva <gnadocursos@gmail.com>
2024-05-14 13:43:44 -0300

    Creat: Footer_and_Footer.module.css

--- Projeto_Start/src/App.jsx
+++ Projeto_Start/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from'./components/Header'
+import Footer from './components/Footer'
 
 function App() {
   
@@ -10,6 +11,8 @@ function App() {
   return (
     <>
       <Header />
+      Conteúdo
+      <Footer />
     </>
   )
 }
---
+++ Projeto_Start/src/components/Footer/index.jsx
@@ -0,0 +1,23 @@
+import styles from './Footer.module.css'
+
+
+
+function Footer(){
+    return(
+        
+        <footer className = {styles.footer}>
+            <nav> 
+                <a href= "">Home</a>
+                <a href= "">Seja Parceiro</a>
+                <a href= "">Contatos</a>
+                <span> 2024 todos os direitos reservados </span>
+            </nav>
+
+
+        </footer>
+
+    )
+
+}
+
+export default Footer
