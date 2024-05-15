b1080409020c9b9bcb33b679c0b0a6d6cf5d6a0a
Isabele Vitória <163940085+isanunesv@users.noreply.github.com>
2024-05-15 19:38:33 -0300

    page cadastro

--- Projeto_Start/src/App.jsx
+++ Projeto_Start/src/App.jsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from'./components/Header'
-import Footer from './components/Footer'
+import Header from "./components/Header/Index"
+import Footer from "./components/Footer"
 
 function App() {
   
---
+++ Projeto_Start/src/pages/Home/cadastroMercado.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import Header from "../../components/Header/Index"
+import Styles from "./cadastroMercado.module.jsx"
+
+
+function cadastroMercado(){
+
+
+
+    return (
+
+  <div className="container">
+    <div className="container-login">
+        <div className="wrap-login">
+            <form className="login-form">
+                <span className="login-form-title">Bem-vindo</span>
+
+                <span className="login-form-title">
+                    <img src="" alt="" />
+                </span>
+                <div className="wrap-input">
+                 <input>
+                 
+                 
+                 </input>                                             
+                </div>
+            </form>
+        </div>
+    </div>
+  </div>
+    
+
+
+   
+)
+
+
+}
