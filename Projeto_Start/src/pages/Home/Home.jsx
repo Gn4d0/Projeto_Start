c4faf3f3c6f7dff31892b76da22d8ab51fff8985
Agnaldo Silva <gnadocursos@gmail.com>
2024-05-15 12:29:38 -0300

    Creat elemts for Home: section parceiros

--- Projeto_Start/src/pages/Home/Home.jsx
+++ Projeto_Start/src/pages/Home/Home.jsx
@@ -15,14 +15,30 @@ function Home() {
           <h1>Onde a economia encontra qualidade!</h1>
           <span>Acessível para todos, graças à nossa parceria com supermercados locais</span>
         </div>
+        
         <img src={mulherIdosa} alt="imagem de home" className='mulherIdosa'/>
+        
         <div>
-        <button calssName = 'promocao' >Promoções</button>
-
+          <button calssName = 'promocao' >Promoções</button>
         </div>
+
+      </section>
+
+      <section className = 'parceiros'>
+          
+          <h2>Paceiros</h2>
+          <span>Atacadão</span>
+          <span>Mix Mateus</span>
+          <span>Novo Atacarejo</span>
+        
+        <div>
+          <button className  = 'mais'>Mais</button>
+        </div>  
       
       </section>
+        
       <Footer />
+
     </>
   )
 }
