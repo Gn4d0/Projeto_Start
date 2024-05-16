325b8f361026d8f3a3e2411f8c1ede87601ed611
isanunesv <isabelen853@gmail.com>
2024-05-16 19:22:37 -0300

    tela de cadastro

--- Projeto_Start/src/pages/Home/Home.jsx
+++ Projeto_Start/src/pages/Home/Home.jsx
@@ -2,14 +2,31 @@ import React from "react"
 import Header from "../../components/Header/Index"
 import Footer from "../../components/Footer"
 import styles from "./home.module.css"
+import modelo from "./img/modelo.jpg"
 
 
 function Home(){
     return (
         <div>
         <Header />
-       
-        <h1 className={styles["title"]}>Home</h1>
+    <div>
+        <div>
+            <img src={modelo} className={styles["modelo"]} alt="" />
+        </div>
+        <div>
+            <h3>Sua Empresa Pede</h3>
+            <p>Bem-vindo ao Mesa Solidária! Somos uma plataforma dedicada a reduzir o desperdício de alimentos e combater a fome, conectando supermercados com produtos próximos à data de vencimento a pessoas em situação de vulnerabilidade social.</p>
+        </div>
+        <div>
+            <form action="">
+                <input type="name" placeholder="Nome" />
+                <input type="email" placeholder="E-mail" />
+                <input type="number" placeholder="Numero de Telefone"/>
+                <button type="submit">Iniciar Cadastro</button>
+            </form>
+            </div>
+        </div>
+        
         <Footer />
         </div>
       
